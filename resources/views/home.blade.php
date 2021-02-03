ebfdf58d882a55b73d4369cdb3c04695fb25acc1
CooleKikker <pctcoen@gmail.com>
2021-02-03 09:44:05 +0100

    Added homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Coen Vink</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="https://use.typekit.net/mdn4bzp.css">
+    <script src="https://kit.fontawesome.com/c20ff3da34.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="homecontainer">
+        <header>
+            <div class="left">
+                <h1><span>C</span>oen <span>V</span>ink</h1>
+            </div>
+            <div class="right">
+                <div class="navdiv">
+                    <i class="fas fa-bars"></i>
+                </div>
+                <nav>
+                    <ul>
+                        <a href="index.html" class="active"><li>Home</li></a>
+                        <a href="over.html"><li>Over Mij</li></a>
+                        <a href="portfolio.php"><li>Portfolio</li></a>
+                        <a href="contact.php"><li>Contact</li></a>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <h2 class="headeranime">Welkom op mijn portfolio-site!</h2>
+    </div>
+</body>
+</html>
